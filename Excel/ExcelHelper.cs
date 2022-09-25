85cf4b87fda63e050be9d0a14fb030fb179c988e
wudisluv <vlad.kornilov2015@gmail.com>
2022-09-25 10:33:04 +0300

    Excel

---
+++ Excel/ExcelHelper.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelHelp.BO
+{
+    class ExcelHelper:  IDisposable
+    {
+        private Application _excel;
+        private Workbook _workbook;
+        public object num;
+        Excel.Range range;
+        public static object[,] range_values;
+        private string _filePath;
+
+        public ExcelHelper()
+        {
+            _excel = new Excel.Application();
+        }
+
+
+        internal bool Open(string filePath)
+        {
+            try
+            {
+               if (File.Exists(filePath))
+                {
+                    _workbook = _excel.Workbooks.Open(filePath);
+                    _filePath = filePath;
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            return true;
+        }
+
+        internal void Save()
+        {
+            if(!string.IsNullOrEmpty(_filePath))
+            {
+                _workbook.SaveAs(_filePath);
+                _filePath = null;
+            }
+            else
+            {
+                _workbook.Save();
+            }
+        }
+
+        internal bool Get(string column, int row)
+        {
+            try
+            {
+                range = (Excel.Range)_excel.Columns[column, Type.Missing];
+                Excel.Range last_cell = range.get_End(Excel.XlDirection.xlDown);
+                Excel.Range first_cell = (Excel.Range)_excel.Cells[row, column];
+                Excel.Range value_range = (Excel.Range)_excel.get_Range(first_cell, last_cell);
+                range_values = (object[,])value_range.Value2;
+                return true;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            return false;
+        }
+        public void Dispose()
+        {
+            try
+            {
+                _workbook.Close();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+    }
---
+++ Excel/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelHelp.BO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (BO.ExcelHelper helper = new BO.ExcelHelper())
+                {
+                    if (helper.Open(filePath: Path.Combine(Environment.CurrentDirectory, "nums.xlsx")))
+                    {
+                        helper.Get(column: "A", row: 1);
+                        helper.Save();
+                    }
+                }
+            }
+            catch(Exception ex) { Console.WriteLine(ex.Message); }
+            string text = null;
+            using (FileStream stream = new FileStream("nums.txt", FileMode.Open))
+            {
+                for (int i = 1; i < ExcelHelper.range_values.Length; i++)
+                {
+                    text = ExcelHelper.range_values[i,1].ToString() + " \r\n";
+                    byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                    stream.Write(array, 0, array.Length);
+                }
+            }
+        }
+    }
+}
